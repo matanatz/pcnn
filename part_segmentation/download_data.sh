e85f8e00fa71b35fdbcad721714e8dc4cb7f3efb
matanatz <matanatz@gmail.com>
2018-09-12 18:06:48 +0300

    part segmentation data script

---
+++ part_segmentation/download_data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Download original ShapeNetPart dataset (around 1GB)
+wget http://www.wisdom.weizmann.ac.il/~haggaim/projects/PCNN/segmentation_data.zip
+unzip segmentation_data.zip
+rm segmentation_data.zip
+
+
